acfe0cb56607554d6c60c68394e11dc44dc37614
Azat Fazlyev <afazlyev436@gmail.com>
2024-01-27 20:18:16 +0100

    Added clear-state-films endpoint

---
+++ clear-state-films/go.mod
@@ -0,0 +1,11 @@
+module finder
+
+go 1.21
+
+require (
+	github.com/aws/aws-lambda-go v1.45.0
+	github.com/aws/aws-sdk-go v1.50.5
+	github.com/google/uuid v1.6.0
+)
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
---
+++ clear-state-films/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/google/uuid"
+	"log"
+)
+
+var sess = session.Must(session.NewSession())
+var db = dynamodb.New(sess)
+
+func main() {
+	lambda.Start(handleRequest)
+}
+
+func handleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	userId, err := getUserIdAndVerify(req)
+	if err != nil {
+		id := req.QueryStringParameters["id"]
+		log.Fatalf("Provided user id is not correct, user id - %s", id)
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Provided user id is not correct, user id - " + id,
+		}, err
+	}
+
+	db.DeleteItem(&dynamodb.DeleteItemInput{
+		TableName: aws.String("user_films"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String(userId),
+			},
+		},
+	})
+	if err != nil {
+		log.Fatalf("Got error calling DeleteItemInput: %s", err)
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       "Got error calling DeleteItemInput: " + err.Error(),
+		}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Success",
+	}, nil
+}
+
+func getUserIdAndVerify(req events.APIGatewayProxyRequest) (string, error) {
+	userId, err := uuid.Parse(req.QueryStringParameters["id"])
+
+	return userId.String(), err
+}
