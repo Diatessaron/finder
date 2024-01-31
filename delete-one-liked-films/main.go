6b95f801c562411f43423c88ae995fcc943f5928
Azat Fazlyev <afazlyev436@gmail.com>
2024-01-31 21:43:33 +0100

    Adjusted AWS DynamoDB to atomic operations for parallel environment

--- delete-one-liked-films/main.go
+++ delete-one-liked-films/main.go
@@ -57,10 +57,9 @@ func handleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (even
 	}
 
 	//remove film from likedFilms and resave others
-	resultLikedFilms := result.Item["likedFilms"].L
-	resultUnlikedFilms := result.Item["unlikedFilms"].L
+	oldLikedFilms := result.Item["likedFilms"].L
 	filmToRemoveIndex := -1
-	for index, film := range resultLikedFilms {
+	for index, film := range oldLikedFilms {
 		if *film.S == filmToRemove {
 			filmToRemoveIndex = index
 		}
@@ -72,23 +71,26 @@ func handleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (even
 			Body:       "Got error removing film - " + ". Film not found.",
 		}, err
 	}
-	resultLikedFilms = append(resultLikedFilms[:filmToRemoveIndex], resultLikedFilms[filmToRemoveIndex+1:]...)
+	resultLikedFilms := append(oldLikedFilms[:filmToRemoveIndex], oldLikedFilms[filmToRemoveIndex+1:]...)
 
-	item := map[string]*dynamodb.AttributeValue{
-		"id": {
-			S: aws.String(userId),
-		},
-		"likedFilms": {
-			L: resultLikedFilms,
-		},
-		"unlikedFilms": {
-			L: resultUnlikedFilms,
-		},
-	}
-
-	_, err = db.PutItem(&dynamodb.PutItemInput{
-		Item:      item,
+	_, err = db.UpdateItem(&dynamodb.UpdateItemInput{
 		TableName: aws.String("user_films"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String(userId),
+			},
+		},
+		ConditionExpression: aws.String("attribute_not_exists(likedFilms) OR likedFilms = :oldVal"),
+		UpdateExpression:    aws.String("SET likedFilms = :val"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":val": {
+				L: resultLikedFilms,
+			},
+			":oldVal": {
+				L: oldLikedFilms,
+			},
+		},
+		ReturnValues: aws.String("UPDATED_NEW"),
 	})
 	if err != nil {
 		log.Fatalf("Got error calling PutItem: %s", err)
--- update-user-films/main.go
+++ update-user-films/main.go
@@ -58,29 +58,43 @@ func handleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (even
 	//create or update user liked/unliked films
 	var resultLikedFilms []*dynamodb.AttributeValue
 	var resultUnlikedFilms []*dynamodb.AttributeValue
+	oldLikedFilms := []*dynamodb.AttributeValue{}
+	oldUnlikedFilms := []*dynamodb.AttributeValue{}
 	if result.Item == nil {
 		resultLikedFilms = userLikedFilm
 		resultUnlikedFilms = userUnlikedFilm
 	} else {
 		resultLikedFilms = append(userLikedFilm, result.Item["likedFilms"].L...)
 		resultUnlikedFilms = append(userUnlikedFilm, result.Item["unlikedFilms"].L...)
+		oldLikedFilms = result.Item["likedFilms"].L
+		oldUnlikedFilms = result.Item["unlikedFilms"].L
 	}
 
-	item := map[string]*dynamodb.AttributeValue{
-		"id": {
-			S: aws.String(userId),
-		},
-		"likedFilms": {
-			L: resultLikedFilms,
-		},
-		"unlikedFilms": {
-			L: resultUnlikedFilms,
-		},
-	}
-
-	_, err = db.PutItem(&dynamodb.PutItemInput{
-		Item:      item,
+	_, err = db.UpdateItem(&dynamodb.UpdateItemInput{
 		TableName: aws.String("user_films"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String(userId),
+			},
+		},
+		ConditionExpression: aws.String("attribute_not_exists(likedFilms) OR attribute_not_exists(unlikedFilms) " +
+			"OR likedFilms = :oldLikedFilms OR unlikedFilms = :oldUnlikedFilms"),
+		UpdateExpression: aws.String("SET likedFilms = :likedFilms, unlikedFilms = :unlikedFilms"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":oldLikedFilms": {
+				L: oldLikedFilms,
+			},
+			":oldUnlikedFilms": {
+				L: oldUnlikedFilms,
+			},
+			":likedFilms": {
+				L: resultLikedFilms,
+			},
+			":unlikedFilms": {
+				L: resultUnlikedFilms,
+			},
+		},
+		ReturnValues: aws.String("UPDATED_NEW"),
 	})
 	if err != nil {
 		log.Fatalf("Got error calling PutItem: %s", err)
